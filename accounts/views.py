62cb2fe91249219757cf093c3e258a29daaf1297
Raj.Ghag <raj.ghag@wdimails.com>
2026-06-29 15:36:38 +0530

    Expose auth endpoints as AllowAny to unlock in Swagger

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from employees.models import Employee
@@ -18,6 +19,7 @@ from .serializers import LoginSerializer, LoginResponseSerializer
     responses={200: LoginResponseSerializer}
 )
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
 
     email = request.data.get('email')
@@ -78,6 +80,7 @@ from .serializers import SendOTPSerializer, VerifyOTPSerializer, ResetPasswordSe
     responses={200: MessageResponseSerializer}
 )
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def send_otp(request):
 
     email = request.data.get('email')
@@ -132,6 +135,7 @@ def send_otp(request):
     responses={200: MessageResponseSerializer}
 )
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def verify_otp(request):
 
     email = request.data.get('email')
@@ -169,6 +173,7 @@ def verify_otp(request):
     responses={200: MessageResponseSerializer}
 )
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def reset_password(request):
 
     email = request.data.get('email')
