81bd93436c2bed26e507d33278b23e9ff2e929f4
Raj.Ghag <raj.ghag@wdimails.com>
2026-06-29 14:58:46 +0530

    Configure production deployment settings and requirements

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u3xycv7!#5$u8d_^jtn_p-(y*zaccc+hd^k%v!(7n+*dxu8vyn'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-u3xycv7!#5$u8d_^jtn_p-(y*zaccc+hd^k%v!(7n+*dxu8vyn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -71,6 +73,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,6 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -160,5 +166,4 @@ EMAIL_HOST_USER = 'rajghag12728@gmail.com'
 EMAIL_HOST_PASSWORD = 'brabfdpelcjrliru'
 DEFAULT_FROM_EMAIL = 'rajghag12728@gmail.com'
 
-import os
 ENABLE_FACE_VERIFICATION = os.environ.get('ENABLE_FACE_VERIFICATION', 'True').lower() == 'true'
